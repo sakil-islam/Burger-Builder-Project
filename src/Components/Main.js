0fe8eff613fde3ec0c1b069d4ddb388916d82967
Sakil Islam <sakilislam1080@gmail.com>
2021-05-19 16:49:19 +0600

    Project setup Create Navbar & Ingredient

---
+++ src/Components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
+import Header from './Header/Header';
+
+const Main = props => {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <BurgerBuilder />
+            </div>
+        </div>
+    );
+}
+
+export default Main;
