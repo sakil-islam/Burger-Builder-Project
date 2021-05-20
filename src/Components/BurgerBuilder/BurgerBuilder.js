a1f459752951f887d4d6f2c4b9886aa78deff857
Sakil Islam <sakilislam1080@gmail.com>
2021-05-21 02:21:05 +0600

    Showing Price

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -2,32 +2,41 @@ import React, { Component } from 'react';
 import Burger from './Burger/Burger';
 import Controls from './Controls/Controls';
 
+const INGREDIENT_PRICE = {
+    salad: 20,
+    cheese: 40,
+    meat: 90,
+}
+
 export default class BurgerBuilder extends Component {
     state = {
         ingredients: [
             { type: 'salad', amount: 0 },
             { type: 'cheese', amount: 0 },
             { type: 'meat', amount: 0 },
-        ]
+        ],
+        totalPrice: 80,
     }
 
     addIngredientHandle = type => {
         const ingredients = [...this.state.ingredients];
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICE[type];
         for (let item of ingredients) {
             if (item.type === type) item.amount++;
         }
-        this.setState({ ingredients: ingredients });
+        this.setState({ ingredients: ingredients, totalPrice: newPrice });
     }
 
     removeIngredientHandle = type => {
         const ingredients = [...this.state.ingredients];
+        const newPrice = this.state.totalPrice - INGREDIENT_PRICE[type];
         for (let item of ingredients) {
             if (item.type === type) {
                 if (item.amount <= 0) return;
                 item.amount--;
             }
         }
-        this.setState({ ingredients: ingredients });
+        this.setState({ ingredients: ingredients, totalPrice: newPrice });
     }
 
     render() {
@@ -37,6 +46,7 @@ export default class BurgerBuilder extends Component {
                 <Controls
                     ingredientAdded={this.addIngredientHandle}
                     ingredientRemoved={this.removeIngredientHandle}
+                    price = {this.state.totalPrice}
                 />
             </div>
         )
