8f08ac1e1fabbf55ccb462093b6bbf9769dd93d9
Sakil Islam <sakilislam1080@gmail.com>
2021-05-22 00:10:46 +0600

    Show Order Summary

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Burger from './Burger/Burger';
 import Controls from './Controls/Controls';
+import Summary from './Summary/Summary';
+import { Modal, ModalBody, ModalHeader, ModalFooter, Button } from 'reactstrap';
 
-const INGREDIENT_PRICE = {
+const INGREDIENT_PRICES = {
     salad: 20,
     cheese: 40,
     meat: 90,
@@ -16,20 +18,30 @@ export default class BurgerBuilder extends Component {
             { type: 'meat', amount: 0 },
         ],
         totalPrice: 80,
+        modalOpen: false,
+        purchasable: false,
+    }
+
+    updatePurchasable = ingredients => {
+        const sum = ingredients.reduce((sum, element) => {
+            return sum + element.amount;
+        }, 0);
+        this.setState({ purchasable: sum > 0 })
     }
 
     addIngredientHandle = type => {
         const ingredients = [...this.state.ingredients];
-        const newPrice = this.state.totalPrice + INGREDIENT_PRICE[type];
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
         for (let item of ingredients) {
             if (item.type === type) item.amount++;
         }
         this.setState({ ingredients: ingredients, totalPrice: newPrice });
+        this.updatePurchasable(ingredients);
     }
 
     removeIngredientHandle = type => {
         const ingredients = [...this.state.ingredients];
-        const newPrice = this.state.totalPrice - INGREDIENT_PRICE[type];
+        const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
         for (let item of ingredients) {
             if (item.type === type) {
                 if (item.amount <= 0) return;
@@ -37,18 +49,41 @@ export default class BurgerBuilder extends Component {
             }
         }
         this.setState({ ingredients: ingredients, totalPrice: newPrice });
+        this.updatePurchasable(ingredients);
+    }
+
+    toggleModal = () => {
+        this.setState({
+            modalOpen: !this.state.modalOpen
+        })
     }
 
     render() {
         return (
-            <div className="d-flex flex-md-row flex-column">
-                <Burger ingredients={this.state.ingredients} />
-                <Controls
-                    ingredientAdded={this.addIngredientHandle}
-                    ingredientRemoved={this.removeIngredientHandle}
-                    price = {this.state.totalPrice}
-                />
+            <div>
+                <div className="d-flex flex-md-row flex-column">
+                    <Burger ingredients={this.state.ingredients} />
+                    <Controls
+                        ingredientAdded={this.addIngredientHandle}
+                        ingredientRemoved={this.removeIngredientHandle}
+                        price={this.state.totalPrice}
+                        toggleModal={this.toggleModal}
+                        purchasable={this.state.purchasable}
+                    />
+                </div>
+                <Modal isOpen={this.state.modalOpen}>
+                    <ModalHeader>Your Order Summary</ModalHeader>
+                    <ModalBody>
+                        <h5>Total Price: {this.state.totalPrice.toFixed(0)} BDT</h5>
+                        <Summary ingredients={this.state.ingredients} />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="success" onClick={this.toggleModal}>Continue to Checkout</Button>
+                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
             </div>
+
         )
     }
 }
