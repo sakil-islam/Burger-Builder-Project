5062dca901c657ebc649bf55c802772f8ae819b5
Sakil Islam <sakilislam1080@gmail.com>
2021-05-23 09:20:21 +0600

    Saving Order to Database

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 
+import axios from 'axios';
+
+import { connect } from 'react-redux';
+
+const mapStateTopProps = state => {
+    return {
+        ingredients: state.ingredients,
+        totalPrice: state.totalPrice,
+        purchasable: state.purchasable,
+    }
+}
+
 class Checkout extends Component {
     state = {
         values: {
@@ -24,12 +36,26 @@ class Checkout extends Component {
     }
 
     submitHandler = () => {
-        console.log(this.state.values);
+        const order = {
+            ingredients: this.props.ingredients,
+            customer: this.state.values,
+            price: this.props.totalPrice,
+            orderTime: new Date(),
+        }
+        axios.post("https://burger-builder-af364-default-rtdb.firebaseio.com/orders.json", order)
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
     }
 
     render() {
         return (
             <div>
+                <h4 style={{
+                    border: "1px solid grey",
+                    boxShadow: "1px 1px #888888",
+                    borderRadius: "5px",
+                    padding: "20px",
+                }}>Payment: {this.props.totalPrice} BDT</h4>
                 <form style={{
                     border: "1px solid grey",
                     boxShadow: "1px 1px #888888",
@@ -46,11 +72,11 @@ class Checkout extends Component {
                     </select>
                     <br />
                     <Button style={{ backgroundColor: "#D70F64" }} className="mr-auto" onClick={this.submitHandler}>Place Order</Button>
-                    <Button color="secondary" className="ml-2" onClick={this.goBack}>Cancel</Button>
+                    <Button color="secondary" className="ml-1" onClick={this.goBack}>Cancel</Button>
                 </form>
             </div>
         )
     }
 }
 
-export default Checkout;
+export default connect(mapStateTopProps)(Checkout);
